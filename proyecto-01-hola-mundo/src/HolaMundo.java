28d527cfd94a3cefc0c97dee4bba8c084cb96485
20253ds144-png <20253ds144@utez.edu.mx>
2026-01-20 12:53:25 -0600

    Correccion

---
+++ proyecto-01-hola-mundo/src/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("David Linares Noriega");
+        System.out.println("2°E");
+        System.out.println("08/01/2026");
+        System.out.println("Hola Mundo");
+    }
+}
